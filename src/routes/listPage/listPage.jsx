95c48a89d1e39c2c6cbab0b897a302f03af67802
David <morabongs@naver.com>
2024-08-19 21:35:28 +0900

    React Responsive List Page Design.

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,7 +1,25 @@
+import { listData } from "../../lib/dummydata";
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
+
 import "./listPage.scss";
 
 const ListPage = () => {
-  return <div className="listPage">ListPage</div>;
+  const data = listData;
+
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer"></div>
+    </div>
+  );
 };
 
 export default ListPage;
