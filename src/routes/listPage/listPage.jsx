a478bd38c4512cff986de65e2ea7bc1b51a0bd3d
David <morabongs@naver.com>
2024-08-20 18:19:56 +0900

    Completing React Leaflet Map.

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,6 +1,7 @@
 import { listData } from "../../lib/dummydata";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
+import Map from "../../components/map/Map";
 
 import "./listPage.scss";
 
@@ -17,7 +18,9 @@ const ListPage = () => {
           ))}
         </div>
       </div>
-      <div className="mapContainer"></div>
+      <div className="mapContainer">
+        <Map items={data} />
+      </div>
     </div>
   );
 };
