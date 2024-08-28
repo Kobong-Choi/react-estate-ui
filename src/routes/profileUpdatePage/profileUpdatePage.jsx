230c23796c8c7b156315e428a0e5832fe5ed153c
David <morabongs@naver.com>
2024-08-28 17:28:56 +0900

    React Express.js Prisma CRUD.

--- src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,28 +1,74 @@
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest.js";
+
 import "./profileUpdatePage.scss";
 
 function ProfileUpdatePage() {
+  const [error, setError] = useState("");
+  const { currentUser, updateUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    // formData 객체는 폼 데이터의 키-값 쌍을 포함하고 있습니다.
+    // formData 객체를 Object.fromEntries()에 전달하면, 해당 키-값 쌍이 자바스크립트 객체로 변환됩니다.
+    const { username, email, password } = Object.fromEntries(formData);
+    try {
+      const res = await apiRequest.put(`/users/${currentUser.id}`, {
+        username,
+        email,
+        password,
+      });
+
+      updateUser(res.data);
+      navigate("/profile");
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data.message);
+    }
+  };
   return (
     <div className="profileUpdatePage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Update Profile</h1>
           <div className="item">
             <label htmlFor="username">Username</label>
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              defaultValue={currentUser.username}
+            />
           </div>
           <div className="item">
             <label htmlFor="email">Email</label>
-            <input id="email" name="email" type="email" />
+            <input
+              id="email"
+              name="email"
+              type="email"
+              defaultValue={currentUser.email}
+            />
           </div>
           <div className="item">
             <label htmlFor="password">Password</label>
             <input id="password" name="password" type="password" />
           </div>
           <button>Update</button>
+          {error && <span>error</span>}
         </form>
       </div>
       <div className="sideContainer">
-        <img src="" alt="" className="avatar" />
+        <img
+          src={currentUser.avatar || "/noavatar.jpg"}
+          alt=""
+          className="avatar"
+        />
       </div>
     </div>
   );
