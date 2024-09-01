c4537c3688150e221359e737cf92625c3d404dfa
David <morabongs@naver.com>
2024-09-01 21:32:00 +0900

    Fetch Profile Posts.

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,13 @@
-import { listData } from "../../lib/dummydata";
 import Card from "../../components/card/Card";
 
 import "./list.scss";
 
-const List = () => {
+const List = ({ posts }) => {
   return (
     <div className="list">
-      {/* {listData.map((item) => (
+      {posts.map((item) => (
         <Card key={item.id} item={item} />
-      ))} */}
+      ))}
     </div>
   );
 };
